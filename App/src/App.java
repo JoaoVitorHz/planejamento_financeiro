4096d52ecacd3bb6524ad2cd35ff8c517fde4f4c
João Vitor <vitorjoao39207@gmail.com>
2023-05-13 05:45:41 -0300

    Exercicio Pronto

--- App/src/App.java
+++ App/src/App.java
@@ -3,41 +3,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
        
-        double menor = 0;
-        double maior = 0;
-        int homem = 0; 
-        int mulher = 0;
-        double alturaHomem = 0;
-
-        for(int i = 0; i <= 10; i++){
             Scanner entrada = new Scanner(System.in);
 
-            System.out.println("Informe a altura dessa pessoa!");
-            double altura = entrada.nextDouble();
+            System.out.println("Informe uma temperatura em celsius");
+            double temperatura = entrada.nextDouble();
 
-            System.out.println("Informe se essa pessoa é homem ou mulher, digite 1 para homem e 2 para mulher");
-            int tipoPessoa = entrada.nextInt();
-
-            if(tipoPessoa == 1){
-                homem ++;
-                alturaHomem += altura;
-            } 
-            else if(tipoPessoa == 2){
-                mulher ++;
-            }
-
-            if(menor == 0){
-                menor = altura;
-            }
-            else if(altura > maior){
-                maior = altura;
-            } 
-            else if(altura < menor){
-                menor = altura;
-            }
-        }
-        System.out.println("A maior altura foi de: " + maior + ". E a menor altura foi de: " + menor);
-        System.out.println("A media de altura dos homens é de: " + (alturaHomem / homem));
-        System.out.println("A quantidade de mulheres é de: " + mulher);
+            System.out.println("Kelvim = " + (temperatura + 273.15));
+            System.out.println("Réaumur  = " + (temperatura * 0.8));
+            System.out.println("Rankine  = " + (temperatura  * 1.8));
+            System.out.println("Fahrenheit   = " + (temperatura  * 1.8 + 32));
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Olá, esse é um exercicio de logica de programação usando Java que recebi na faculdade e decidi colocar aqui no gitHub. 
 
 ## Enunciado do Exercicio
-2) Tem-se um conjunto de dados contendo a altura e o sexo (masculino, feminino) de 10 pessoas. Fazer um algoritmo que calcule e escreva:
-- a) a maior e a menor altura do grupo;
-- b) média de altura dos homens;
-- c) o número de mulheres.
+3) Escreva um programa que, com base em uma temperatura em graus celsius, a converta e exiba em Kelvin (K), Réaumur (Re), Rankine (Ra) e Fahrenheit (F), seguindo as fórmulas: 
+- F = C * 1.8 + 32; 
+- K = C + 273.15; 
+- Re = C * 0.8; 
+- Ra = C * 1.8 + 32 + 459.67
